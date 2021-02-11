ec47b5f32c1ef49fdbf3e2723e71c6d65ffeab4e
peng <peng4567@163.com>
2021-02-11 13:04:32 +0800

    log4j.xml not working

--- src/main/java/org/peng/blog/LoginController.java
+++ src/main/java/org/peng/blog/LoginController.java
@@ -1,15 +1,30 @@
 package org.peng.blog;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 public class LoginController {
+    private static final Logger LOGGER = LogManager.getLogger(LoginController.class);
 
-    @RequestMapping(params = "login",method=RequestMethod.GET)
-    public String doLogin(ModelMap model) { // 登录验证
+    @RequestMapping(value = "login")
+    public ModelAndView doLogin(ModelMap model, String username, String pwd) { // 登录验证
+        System.out.println("come to loginController");
+        LOGGER.info("logger info");
+        LOGGER.error("logger error");
+        LOGGER.debug("logger debug");
         model.addAttribute("message", "Spring 3 MVC Hello World");
-        return "hello";
+        model.addAttribute("username",username);
+        model.addAttribute("pwd",pwd);
+        //return "views/hello";
+        ModelAndView mv = new ModelAndView("hello");
+        System.out.println("username : " + username + " " + pwd);
+        return mv;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
+<head>
+    <title>Start Page</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
 <body>
-<h2>Hello World!</h2>
+<h1>Hello World!</h1>
+<a href="login.do?username=123&pwd=abc">go login</a><br>
 </body>
 </html>
